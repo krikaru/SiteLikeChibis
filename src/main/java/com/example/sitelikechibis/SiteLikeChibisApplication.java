a3a58c2a3c665bbf22dc5c31d04f53f503c9b11a
Соkrikaru <lesoleill64@gmail.com>
2021-12-13 14:33:39 +0300

    init app + webpack

---
+++ src/main/java/com/example/sitelikechibis/SiteLikeChibisApplication.java
@@ -0,0 +1,13 @@
+package com.example.sitelikechibis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteLikeChibisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteLikeChibisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sitelikechibis/SiteLikeChibisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sitelikechibis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteLikeChibisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
